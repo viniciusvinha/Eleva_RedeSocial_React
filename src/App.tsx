c69ae5a02b8de98843429b8b4fb5b05eb5b37e1e
Lara Daniel <laradanielt@gmail.com>
2022-01-28 16:22:27 -0300

    implementação métodos get e post

--- src/App.tsx
+++ src/App.tsx
@@ -7,33 +7,69 @@ import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import Inicio from './paginas/inicio/Inicio';
 import './App.css';
-
+import ListaTema from './componentes/temas/listaTema/ListaTema';
+import ListaPostagem from './componentes/postagens/listaPostagem/ListaPostagem';
+import CadastroPost from './componentes/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './componentes/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './componentes/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './componentes/temas/deletarTema/DeletarTema';
 
 
 function App() {
   return (
     <Router>
       <Navbar />
-        <Switch>
-          <div style={{minHeight: '100vh'}}>
+      <Switch>
+        <div style={{ minHeight: '100vh' }}>
 
-            <Route exact path='/'>
-              <Inicio />
-            </Route>
+          <Route exact path='/'>
+            <Login />
+          </Route>
 
-            <Route path='/login'>
-              <Login />
-            </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
 
-            <Route path='/home'>
-              <Home />
-            </Route>
-            
-            <Route path='/cadastrousuario'>
-              <CadastroUsuario />
-            </Route>
-          </div>
-        </Switch>
+          <Route path='/home'>
+            <Home />
+          </Route>
+
+          <Route path='/inicio'>
+            <Inicio />
+          </Route>
+
+          <Route path='/cadastrousuario'>
+            <CadastroUsuario />
+          </Route>
+          <Route path='/temas'>
+            <ListaTema />
+          </Route>
+          <Route path='/posts'>
+            <ListaPostagem />
+          </Route>
+
+          <Route exact path='/formularioPostagem'>
+            <CadastroPost />
+          </Route>
+          <Route exact path='/formularioPostagem/:id'>
+            <CadastroPost />
+          </Route>
+          <Route exact path='/formularioTema'>
+            <CadastroTema />
+          </Route>
+          <Route exact path='/formularioTema/:id'>
+            <CadastroTema />
+          </Route>
+          <Route path='/deletarPostagem/:id'>
+            <DeletarPostagem />
+          </Route>
+          <Route path='/deletarTema/:id'>
+            <DeletarTema />
+          </Route>
+
+
+        </div>
+      </Switch>
       <Footer />
     </Router>
   );
