e0882aaa66c9fd1f2067c3e6af7dfe0bc0a35482
Lara Daniel <laradanielt@gmail.com>
2022-01-31 14:47:57 -0300

    estilização página início

--- src/paginas/inicio/Inicio.tsx
+++ src/paginas/inicio/Inicio.tsx
@@ -23,7 +23,7 @@ function Inicio() {
                     <Box>
                         <img src={require('../../assets/img/Logo Completo (9).png')} alt="" className='img-inicio' />
                     </Box>
-                    <Box>
+                    <Box className='paddingTop2'>
                         <Typography variant='h6' color='textPrimary' component='h3' align='center' style={{color: "#4A2B87"}}>Aqui uma frase de efeito muito legal</Typography>
                         <Typography variant='h6' color='textPrimary' component='h3' align='center' style={{color: "#4A2B87"}}>sobre mulheres e tecnologia!</Typography>
                     </Box>
@@ -43,6 +43,28 @@ function Inicio() {
                     <Grid item xs={6}>
                         <img src={require('../../assets/img/img-inicio.png')} alt="" className='img-sobre1' />
                     </Grid>
+                </Grid>
+                <Grid container className='caixa3'>
+                    <Grid item xs={4} className='padding'>
+                        <Typography variant='h5' gutterBottom className='titulo-sobre'>Sobre a ODS 5</Typography>
+                        <Typography variant='body1' className='paddingTop2'>Dados relevantes sobre mulheres ocupando vagas como devs. Dados ocupando vagas como desenvolvedoras.</Typography>
+                        <Typography variant='body1' className='paddingTop2'>Importância de incentivar a aprenderem sobre programação e ocuparem estes espaços. importância de incentivar a aprenderem sobre programação e ocuparem estes espaços.</Typography>
+                        <Typography variant='body1' className='paddingTop2'>Importância de incentivar a aprenderem sobre programação e ocuparem estes espaços. importância de incentivar a aprenderem sobre programação e ocuparem estes espaços.</Typography>
+                    </Grid>
+                    <Grid item xs={4} className='padding'>
+                        <Typography variant='subtitle2' align='center' className='titulo-sobre'>Título do Gráfico</Typography>
+                        <Typography variant='body2' align='center'>subtítulo do gráfico de pizza muito interessante</Typography>
+                    </Grid>
+                    <Grid item xs={4} className='padding'>
+                        <Typography variant='subtitle2' align='center' className='titulo-sobre'>Título do Gráfico</Typography>
+                        <Typography variant='body2' align='center'>subtítulo do gráfico de barras muito interessante</Typography>
+                    </Grid>
+                </Grid>
+                <Grid container className='caixa4'>
+                    <Box className="box-equipe">
+                        <Typography variant='h5' className='titulo-equipe'>Desenvolvedores</Typography>
+                        <img src={require('../../assets/img/logo-equipe.png')} alt="" className='logo-equipe'/>
+                    </Box>
                 </Grid>
                
             </Grid>
