7f960664eda186fad6662a743e5f7627551b2952
ViniciusVinha <vinicius_vinha@hotmail.com>
2022-02-02 18:39:19 -0300

    Alteração de fonte

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -78,7 +78,7 @@ function Login() {
                     
                     <Box paddingX={20}>
                         <form onSubmit={onSubmit}>
-                            <Typography variant='h6' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entre com o seu Login:</Typography>
+                            <Typography variant='h6' gutterBottom color='textPrimary' component='h3' align='center' className='textos1 font-link'>Entre com o seu Login:</Typography>
                             <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
                             <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                             <Box marginTop={2} textAlign='center'>
